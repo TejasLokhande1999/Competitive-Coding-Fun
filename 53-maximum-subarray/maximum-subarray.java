2b17539b20e76fd6f611d9c659f36a7bd66469aa
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2024-07-31 04:03:23 -0400

    Time: 1 ms (99.59%) | Memory: 57.3 MB (18.19%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -17,22 +17,49 @@ class Solution {
         // }
         // return maxSum;
 
-        if(nums.length==1){
-            return nums[0];
+
+
+
+
+
+    //     if(nums.length==1){
+    //         return nums[0];
+    //     }
+
+    //     int maxSum=Integer.MIN_VALUE;
+    //     int currSum=0;
+
+    //     for(int i=0;i<nums.length;i++){
+    //         currSum+=nums[i];
+    //         if(currSum>maxSum){
+    //             maxSum = currSum;
+    //         }
+    //         if(currSum<0){      //Set it to 0 as it makes no sense to carry a -ve number forward. It will reduce the overall value
+    //             currSum=0;
+    //         }
+    //     }
+
+    // return maxSum;
+
+
+    if(nums.length==1){
+        return nums[0];
+    }
+
+    int currSum=0;
+    int maxSum = Integer.MIN_VALUE;
+    for(int i=0;i<nums.length;i++){
+        currSum+= nums[i];
+
+        if(currSum>maxSum){
+            maxSum = currSum;
         }
 
-        int maxSum=Integer.MIN_VALUE;
-        int currSum=0;
-
-        for(int i=0;i<nums.length;i++){
-            currSum+=nums[i];
-            if(currSum>maxSum){
-                maxSum = currSum;
-            }
-            if(currSum<0){      //Set it to 0 as it makes no sense to carry a -ve number forward. It will reduce the overall value
-                currSum=0;
-            }
+        if(currSum<0){
+            currSum=0;
         }
+    }
+
 
     return maxSum;
 
