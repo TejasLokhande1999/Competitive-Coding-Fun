528fad30a791570f0d87a5d205b81a2512d26971
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2024-08-12 23:45:21 -0400

    Time: 0 ms (100.00%) | Memory: 42.1 MB (82.48%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,6 +1,8 @@
 class Solution {
     public int findPeakElement(int[] arr) {
         
+        int n = arr.length;
+
         if(arr.length==1){
             return 0;
         }
@@ -9,40 +11,83 @@ class Solution {
             return 0;
         }
 
-        int n = arr.length;
-
         if(arr[n-1]>arr[n-2]){
             return n-1;
-        }
+        }else{
 
-        // for(int i=1;i<n-1;i++){
-        //     if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
-        //         return i;
-        //     }
-        // }
+            int start = 1;
+            int end = n-2;
 
-        // return -1;
+            while(start<=end){
 
-        int start =1;
-        int end =n-2;
+                int mid = start + (end-start)/2;
 
-        while(start<=end){
-            int mid = start + (end-start)/2;
+                if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
+                    return mid;
+                }else if(arr[mid]<arr[mid+1]){
 
-            if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
-                return mid;
-            }else if(arr[mid-1]< arr[mid] && arr[mid]<arr[mid+1]){
-                //peak element at right
+                        start =mid+1;
 
-                start = mid+1;
-            }else{
-                end = mid-1;
+                }else{
+                    end = mid-1;
+
+                }
+
+
             }
 
+            return -1;
+
+
         }
 
 
-    return -1;
+
+
+
+
+    //     if(arr.length==1){
+    //         return 0;
+    //     }
+
+    //     if(arr[0]>arr[1]){
+    //         return 0;
+    //     }
+
+    //     int n = arr.length;
+
+    //     if(arr[n-1]>arr[n-2]){
+    //         return n-1;
+    //     }
+
+    //     // for(int i=1;i<n-1;i++){
+    //     //     if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
+    //     //         return i;
+    //     //     }
+    //     // }
+
+    //     // return -1;
+
+    //     int start =1;
+    //     int end =n-2;
+
+    //     while(start<=end){
+    //         int mid = start + (end-start)/2;
+
+    //         if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
+    //             return mid;
+    //         }else if(arr[mid-1]< arr[mid] && arr[mid]<arr[mid+1]){
+    //             //peak element at right
+
+    //             start = mid+1;
+    //         }else{
+    //             end = mid-1;
+    //         }
+
+    //     }
+
+
+    // return -1;
 
     }
 }
