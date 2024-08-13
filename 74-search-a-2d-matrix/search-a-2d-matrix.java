7d9000fc3ff96515ab12aff30ea5b4db24210132
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2024-08-13 00:45:52 -0400

    Time: 0 ms (100.00%) | Memory: 41.8 MB (77.63%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,29 +1,57 @@
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int ele) {
+    public boolean searchMatrix(int[][] matrix, int target) {
 
-       int n = matrix.length;
-       int m = matrix[0].length;
+        int m = matrix.length;      //no of rows
 
-       int start = 0;
-       int end = n*m -1;
+        int n = matrix[0].length;   //no of cols
 
-       while(start<=end){
-        int mid = start+(end-start)/2;
+        int start=0;
+        int end = m*n-1;
 
-        int row = mid/m;
-        int col = mid%m;
+        while(start<=end){
+            int mid = start + (end-start)/2;
 
-        if(matrix[row][col]==ele){
-            return true;
-        }else if(matrix[row][col]<ele){
-            start = mid+1;
-        }else{
-            end = mid-1;
+            int row = mid/n;
+            int col = mid%n;
+
+            if(matrix[row][col]==target){
+                return true;
+            }else if(matrix[row][col]<target){
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }
+            
         }
 
-       } 
+    return false;
 
-       return false;
+
+
+
+    //    int n = matrix.length;
+    //    int m = matrix[0].length;
+
+    //    int start = 0;
+    //    int end = n*m -1;
+
+    //    while(start<=end){
+    //     int mid = start+(end-start)/2;
+
+    //     int row = mid/m;
+    //     int col = mid%m;
+
+    //     if(matrix[row][col]==ele){
+    //         return true;
+    //     }else if(matrix[row][col]<ele){
+    //         start = mid+1;
+    //     }else{
+    //         end = mid-1;
+    //     }
+
+    //    } 
+
+    //    return false;
        
 
     }
