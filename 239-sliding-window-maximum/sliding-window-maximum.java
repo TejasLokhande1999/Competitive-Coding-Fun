eb9df9428ea536d3901a9d0b897ca06bcb65d15f
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2024-08-22 08:50:25 -0400

    Time: 31 ms (62.04%) | Memory: 62.3 MB (20.43%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -8,48 +8,22 @@ class Solution {
 
         while(j<n){
 
-            // while(!q.isEmpty() && q.peekLast()<arr[j]){
-            //     q.pollLast();
-            // }
-
-            // q.push(arr[j]);
-
-            // if(j-i+1<k){
-            //     j++;
-            // }else if(j-i+1==k){
-            //     if(q.peek()==arr[i]){
-            //         ans.add(q.peek());
-            //         q.poll();
-            //     }else{
-            //         ans.add(q.peek());
-            //     }
-            //     i++;
-            //     j++;
-            // }
-
-                    // Maintain decreasing order in deque and store values
-        while (!q.isEmpty() && q.peekLast() < arr[j]) {
-            q.pollLast();
-        }
-
-        // Add the current element to the deque
-        q.offerLast(arr[j]);
-
-        if (j - i + 1 < k) {
-            j++;
-        } else if (j - i + 1 == k) {
-            // The element at the front of the deque is the maximum in the current window
-            ans.add(q.peekFirst());
-
-            // Remove the element at index i if it is the maximum and out of the window
-            if (q.peekFirst() == arr[i]) {
-                q.pollFirst();
+            while(!q.isEmpty() && q.peekLast()<arr[j]){
+                q.pollLast();
             }
 
-            // Slide the window
-            i++;
-            j++;
-        }
+            q.offerLast(arr[j]);
+
+            if(j-i+1<k){
+                j++;
+            }else if(j-i+1==k){
+                ans.add(q.peek());
+                if(q.peek()==arr[i]){
+                    q.pollFirst();
+                }
+                i++;
+                j++;
+            }
 
         }
 
