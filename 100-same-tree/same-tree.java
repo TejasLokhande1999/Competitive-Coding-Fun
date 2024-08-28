70d82292c6cc931acb4af09418e1dec64fb28347
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2024-08-28 11:20:17 -0400

    Time: 0 ms (100.00%) | Memory: 41.4 MB (6.53%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -15,29 +15,19 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        
-        return inorder(p,q);
-    }
-
-    public boolean inorder(TreeNode p, TreeNode q){
-
         if(p==null && q==null){
-                return true;
+            return true;
         }
 
         if(p==null || q==null){
             return false;
         }
 
-        if(p.val==q.val){
+        if(p.val == q.val){
             
-            return inorder(p.left,q.left) && inorder(p.right,q.right);
+            return isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
+
         }
-
         return false;
-     
-
-
-
     }
 }
