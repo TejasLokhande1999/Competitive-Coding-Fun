2687479204f68adbe18855f72a4bb51c6d7e81f9
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2024-08-28 11:27:02 -0400

    Time: 0 ms (100.00%) | Memory: 41.7 MB (38.83%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.java
+++ 101-symmetric-tree/symmetric-tree.java
@@ -15,29 +15,21 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        
-        return isMirror(root.left,root.right);
+        return func(root.left,root.right);
     }
 
-    public boolean isMirror(TreeNode node1, TreeNode node2){
-
-        if(node1==null && node2==null)
-        {
+    public boolean func(TreeNode p, TreeNode q){
+        if(p==null && q==null){
             return true;
         }
 
-        if(node1==null || node2==null)
-        {
+        if(p==null || q==null){
             return false;
         }
 
-        if(node1.val==node2.val){
-
-            return isMirror(node1.left, node2.right) && isMirror(node1.right, node2.left);
+        if(p.val==q.val){
+            return func(p.left,q.right) && func(p.right,q.left);
         }
-
         return false;
-
-
     }
 }
