6e18389cd320b9e57feb026b1d70589a882ee9ee
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2024-08-29 16:56:48 -0400

    Time: 4 ms (47.97%) | Memory: 49.6 MB (37.72%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,100 +1,36 @@
-class Pair{
-    int row;
-    int col;
-
-    Pair(int row, int col){
-        this.row=row;
-        this.col=col;
-    }
-
-}
-
 class Solution {
     public int numIslands(char[][] grid) {
-        
-
         int m = grid.length;
         int n = grid[0].length;
         boolean visited[][] = new boolean[m][n];
-
         int counter=0;
-
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]=='1' && visited[i][j]==false){
-                    //apply bfs
-                    bfs(i,j,grid,visited,m,n);
+                if(grid[i][j]=='1' && !visited[i][j]){
+                    dfs(i,j,visited,grid);
                     counter++;
                 }
             }
         }
 
         return counter;
-
     }
 
-// public void bfs(int i, int j, char grid[][], boolean visited[][], int m, int n) {
-//     Queue<Pair> q = new LinkedList<>();
-//     q.add(new Pair(i, j));
-//     visited[i][j] = true;  // Mark as visited when adding to the queue
+    public void dfs(int row, int col, boolean visited[][], char grid[][]){
 
-//     // Directions array for moving up, down, left, right
-//     int[] dirRow = {-1, 1, 0, 0};
-//     int[] dirCol = {0, 0, -1, 1};
+        int m = grid.length;
+        int n = grid[0].length;
 
-//     while (!q.isEmpty()) {
-//         Pair node = q.poll();
-//         int row = node.row;
-//         int col = node.col;
-
-//         // Explore neighbors in four possible directions
-//         for (int k = 0; k < 4; k++) {
-//             int checkRow = row + dirRow[k];
-//             int checkCol = col + dirCol[k];
-
-//             // Check if the new position is within bounds, is land ('1'), and not visited yet
-//             if (checkRow >= 0 && checkRow < m && checkCol >= 0 && checkCol < n && grid[checkRow][checkCol] == '1' && !visited[checkRow][checkCol]) {
-//                 q.add(new Pair(checkRow, checkCol));
-//                 visited[checkRow][checkCol] = true;  // Mark as visited when adding to the queue
-//             }
-//         }
-//     }
-// }
-
-
-    public void bfs(int i, int j, char grid[][], boolean visited[][], int m, int n){
-
-        Queue<Pair> q = new LinkedList<>();
-
-        q.add(new Pair(i,j));
-        visited[i][j] = true;
-
-        int[] nrow = {-1, 1, 0, 0};
-        int[] ncol = {0, 0, -1, 1};
-
-        while(!q.isEmpty()){
-
-            Pair node = q.poll();
-            int row = node.row;
-            int col = node.col;
-
-            for(int k =0; k<4;k++){
-    
-                     int checkRow = nrow[k] + row;
-                     int checkCol = ncol[k] + col;
-
-                    if(checkRow>=0 && checkRow<m && checkCol>=0 && checkCol<n && grid[checkRow][checkCol]=='1' && !visited[checkRow][checkCol]){
-
-                      //    visited[checkRow][checkCol] = true;
-                        q.add(new Pair(checkRow,checkCol));
-                                    visited[checkRow][checkCol] = true;
-
-                    }
-                
-            }
-
+        if(row<0 || row>=m || col<0 || col>=n || grid[row][col]=='0' || visited[row][col]){         //already visited
+            return;
         }
 
+        visited[row][col]=true;
+
+        dfs(row+1,col,visited,grid);
+        dfs(row-1,col,visited,grid);
+        dfs(row,col+1,visited,grid);
+        dfs(row,col-1,visited,grid);
 
     }
 }
