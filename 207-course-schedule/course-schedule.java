846af33cf2c6a79e5e88783681d3dd7e7971afcb
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2024-08-30 09:39:02 -0400

    Time: 7 ms (30.78%) | Memory: 44.4 MB (68.62%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,63 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prereq) {
+        int V = numCourses;
+
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0;i<V;i++){
+            adj.add(new ArrayList<>());
+        }
+
+        int m = prereq.length;
+        //create adj
+        for(int i=0;i<m;i++){
+            int u = prereq[i][1];
+            int v = prereq[i][0];
+
+            adj.get(u).add(v);
+
+        }
+
+        int indegree[] = new int[V];
+
+        for(int i=0;i<adj.size();i++){
+            List<Integer> list = adj.get(i);
+            for(int j=0;j<list.size();j++){
+                indegree[list.get(j)]+=1;
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0;i<indegree.length;i++){
+            if(indegree[i]==0){
+                q.add(i);
+            }
+        }
+
+        List<Integer> topo = new ArrayList<>();
+        while(!q.isEmpty()){
+
+            int currNode = q.poll();
+            topo.add(currNode);
+
+            List<Integer> temp = adj.get(currNode);
+            for(int i=0;i<temp.size();i++){
+                int node = temp.get(i);
+
+                indegree[node]-=1;
+
+                if(indegree[node]==0){
+                    q.add(node);
+                }
+            }
+
+        }
+
+        if(topo.size()==V){
+            return true;
+        }
+
+        return false;
+
+
+    }
+}
