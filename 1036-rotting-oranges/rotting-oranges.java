748e1e0e7a8684e9de4a48d1410eff59db48e3db
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2024-08-30 03:12:15 -0400

    Time: 2 ms (71.32%) | Memory: 42.9 MB (13.77%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,40 +1,16 @@
-class Pair{
-
-    int row;
-    int col;
-    int time;
-
-    Pair(int row, int col, int time){
-        this.row = row;
-        this.col=col;
-        this.time=time;
-    }
-
-}
-
 class Solution {
     public int orangesRotting(int[][] grid) {
         
+        Queue<Pair> q = new LinkedList<>();
         int m = grid.length;
         int n = grid[0].length;
 
-        if(m==1 && n==1){
-            if(grid[0][0]==0){
-                return 0;
-            }
-            if(grid[0][0]==1){
-                return -1;
-            }
-        }
-
         boolean visited[][] = new boolean[m][n];
-
-        Queue<Pair> q = new LinkedList<Pair>();
-
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==2){
-                    q.add(new Pair(i,j,0));         //put time =0;
+                    q.add(new Pair(i,j,0));
+                    visited[i][j]=true;
                 }
             }
         }
@@ -43,30 +19,34 @@ class Solution {
 
         while(!q.isEmpty()){
             Pair p = q.poll();
-
-            int row = p.row;
-            int col = p.col;
+            int row =p.row;
+            int col =p.col;
             int time = p.time;
-            tmax = Math.max(tmax,time);
+
+            tmax= Math.max(tmax,time);
 
             int nRow[] = {-1,0,1,0};
-            int nCol[]= {0,1,0,-1};
+            int nCol[] = {0,1,0,-1};
 
             for(int k=0;k<4;k++){
-                int checkRow = row + nRow[k];
-                int checkCol = col + nCol[k];
+                int newRow = row+nRow[k];
+                int newCol = col+nCol[k];
 
-                if(checkRow>=0 && checkRow<m && checkCol>=0 && checkCol<n && grid[checkRow][checkCol]==1){
-                    grid[checkRow][checkCol]=2;
-                    q.add(new Pair(checkRow,checkCol,time+1));
+                //check the condition
+                if(newRow>=0 && newRow<m && newCol>=0 && newCol<n && !visited[newRow][newCol] && grid[newRow][newCol]==1){
+                    //then visit
+                    //we are visiting the unvisited 1 node
+                    q.add(new Pair(newRow, newCol, time+1));
+                    visited[newRow][newCol]=true;
                 }
             }
 
         }
 
+
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]==1){
+                if(grid[i][j]==1 && visited[i][j]==false){
                     return -1;
                 }
             }
@@ -74,5 +54,22 @@ class Solution {
 
         return tmax;
 
+
+
+
     }
+}
+
+class Pair{
+
+    int row;
+    int col;
+    int time;
+
+    Pair(int row, int col, int time){
+        this.row =row;
+        this.col=col;
+        this.time = time;
+    }
+
 }
