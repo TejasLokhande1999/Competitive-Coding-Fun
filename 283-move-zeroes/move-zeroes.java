12ae84653c26e7a6a2514495fade755053e7e97c
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2024-09-02 12:19:40 -0400

    Time: 2 ms (82.89%) | Memory: 46 MB (45.99%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,57 +1,35 @@
 class Solution {
-    public void moveZeroes(int[] nums) {
-        
-        //Brute force
-
-        // int arr2[] = new int[nums.length];
-
-        // int index=0;
-        // for(int i=0;i<nums.length;i++){
-        //     if(nums[i]!=0){
-        //         arr2[index] = nums[i];
-        //         index++;
-        //     }
-        // }
-
-        // for(int i=index;i<arr2.length;i++){
-        //     arr2[i] = 0;
-        // }
-
-        // for(int i=0;i<nums.length;i++){
-        //     nums[i] = arr2[i];
-        // }
-
-
-    //2 pointer approach
-
-    
-        
-    int i=-1;
-
-    if(nums.length==1){
-        return;
-    }
-
-    for(int j=0;j<nums.length;j++){
-        if(nums[j]==0){
-            i=j;
-            break;
+    public void moveZeroes(int[] arr) {
+        //check for i
+        int i=0;
+        int n = arr.length;
+        if(n==0){
+            return;
         }
-    }
-
-    if(i==-1){
-        return;
-    }
-
-    for(int j=i+1;j<nums.length;j++){
-        if(nums[j]!=0){
-            //swap
-            int temp = nums[j];
-            nums[j]= nums[i];
-            nums[i] = temp;
-            i++;
+        boolean avl=false;
+        for(int k=0;k<n;k++){
+            if(arr[k]==0){
+                i=k;
+                avl=true;
+                break;
+            }
         }
-    }
+
+        if(!avl){
+            return;
+        }
+
+
+        for(int j=i+1;j<n;j++){
+            if(arr[j]!=0){
+                int temp = arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+                i++;
+            }
+        }
+
+
 
     }
 }
