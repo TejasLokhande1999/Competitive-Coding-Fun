3aa8fb2b4d02af4e17c05f50d220493f4972facc
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2024-09-02 13:47:30 -0400

    Time: 2 ms (98.34%) | Memory: 44.6 MB (89.31%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,82 +1,19 @@
 class Solution {
-    public int[] twoSum(int[] nums, int k) {
-
-        // HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-
-        // int retArr[] = new int[2];
-        // for(int i=0;i<nums.length;i++){
-        //     if(map.containsKey(k-nums[i])){     //we have the target-ele
-        //         retArr[0] = map.get(k-nums[i]);
-        //         retArr[1] = i;
-
-        //         return retArr;
-
-        //     }
-
-        //     if(!map.containsKey(nums[i])){
-        //         map.put(nums[i],i);
-        //     }
-        // }
-
-        // return retArr;
-
-
-        ArrayList<Pair> list = new ArrayList<Pair>();
-        for(int i=0;i<nums.length;i++){
-            list.add(new Pair(nums[i],i));
-        }
-
-
-        Collections.sort(list, new PairComparator());
-
-        int i=0;
-        int n = nums.length;
-        int j=n-1;
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
 
         int retArr[] = new int[2];
-        while(i<j){
-            int sum = list.get(i).ele + list.get(j).ele;
 
-            if(sum==k){
-                retArr[0] = list.get(i).index;
-                retArr[1] = list.get(j).index;
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(target-nums[i])){
+                retArr[0] = i;
+                retArr[1] = map.get(target-nums[i]);
                 return retArr;
-            }else if(sum>k){
-                j--;
-            }else{  //sum<k
-                i++;
             }
 
+            map.put(nums[i], i);
         }
 
         return retArr;
-
-
-       
-    }
-}
-
-class Pair{
-
-    int ele;
-    int index;
-
-    Pair(int ele, int index){
-        this.ele = ele;
-        this.index=index;
-    }
-
-}
-
-class PairComparator implements Comparator<Pair>{
-
-    public int compare(Pair p1, Pair p2){
-        if(p1.ele>p2.ele){
-            return 1;
-        }else if(p1.ele<p2.ele){
-            return -1;
-        }else{
-            return 0;
-        }
     }
 }
