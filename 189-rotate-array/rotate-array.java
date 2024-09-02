3ba4c8985bb4e179800d05b76e70a87de48d4a64
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2024-09-02 11:55:44 -0400

    Time: 1 ms (50.78%) | Memory: 57.3 MB (51.32%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,49 +1,30 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        
-        //Approach 1 TLE
-
-        // int n = nums.length;
-        // k=k%n;
-        // for(int i=0;i<k;i++){
-
-        //     int last = nums[n-1];
-        //     int temp=nums[0];
-        //     int temp2;
-        //     for(int j=1;j<nums.length;j++){
-
-        //         temp2 = nums[j];
-        //         nums[j]=temp;
-        //         temp = temp2;
-
-        //     }
-        //     nums[0] = last;
-
-        // }
-
-
         int n = nums.length;
         k=k%n;
-        if(n==1){
-            return;
-        }
-        reverse(0,n-k-1,nums);
-        reverse(n-k,n-1,nums);
+        // for(int i=0;i<k;i++){
+        //     int saved = nums[n-1];
+        //     for(int j=n-1;j>=1;j--){
+        //         nums[j]=nums[j-1];
+        //     }
+        //     nums[0]=saved;
+        // }
+        
         reverse(0,n-1,nums);
-        //reverse(0,k-1,nums);
-        //reverse(k,n-1,nums);
-
+        reverse(0,k-1,nums);
+        reverse(k,n-1,nums);
     }
 
-    public void reverse(int start, int end, int[] nums){
+    public void reverse(int start, int end, int arr[])
+    {
 
-            while(start<end){
-                int temp = nums[start];
-                nums[start] = nums[end];
-                nums[end] = temp;
-                start++;
-                end--;
-            }
-
+        while(start<end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
         }
+
+    }
 }
