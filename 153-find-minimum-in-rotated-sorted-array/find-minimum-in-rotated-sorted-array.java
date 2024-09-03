f22a2a42d3a48db08e2b2f8c6988eec3e5fa9335
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2024-09-03 07:27:57 -0400

    Time: 0 ms (100.00%) | Memory: 41.7 MB (76.91%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -1,65 +1,25 @@
 class Solution {
     public int findMin(int[] arr) {
-        
-        int start =0;
+        int start=0;
         int end = arr.length-1;
         int n = arr.length;
 
-        // //base condition
-        // if(nums[start]<nums[end]){
-        //     return nums[start];
-        // }
+        while(start<=end){
+            int mid = start+(end-start)/2;
 
-        // while(start<=end){
-
-        //     int mid = start + (end-start)/2;
-
-        //     if(nums[start]<nums[end]){
-        //     return nums[start];
-        // }
-        //     if(nums[(mid+1)%n]>= nums[mid] && nums[mid]<=nums[(mid + n-1)%n]){
-        //         return nums[mid];
-        //     }
-        //     else if(nums[start]<=nums[mid]){
-        //         //mode right as we are in sorted array
-        //         start = mid+1;
-        //     }else if(nums[mid]<=nums[end]){
-        //         end = mid-1;
-        //     }
-
-        // }
-
-        // return 0;
-
-
-        int min = Integer.MAX_VALUE;
-        if(arr[0]<=arr[n-1]){
-            return arr[0];
-        }else{
-
-            while(start<=end){
-
-                int mid = start + (end-start)/2;
-
-                if(arr[start]<=arr[mid]){
-                    min = Math.min(min,arr[start]);
-                    //go to unsorted portion
-                    start = mid+1;
-                }else{
-                    min = Math.min(min,arr[mid]);
-                    end = mid-1;
-                }
+            if(arr[start]<arr[end]){
+                return arr[start];
             }
-
-
-
-
+            if(arr[mid]<=arr[(mid-1+n)%n] && arr[mid]<=arr[(mid+1)%n]){
+                return arr[mid];
+            }else if(arr[mid]>=arr[start]){
+                start = mid+1;   //go to unsorted portion
+            }else{
+                end = mid-1;
+            }
         }
 
-
-
-            return min;
-
+        return -1;
 
     }
 }
