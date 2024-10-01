3a4d5cea3d92e5883425408e82ac568ef0ead60c
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2024-10-01 10:25:19 -0400

    Time: 0 ms (100.00%) | Memory: 41.5 MB (76.50%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,19 +1,34 @@
 class Solution {
     public int numDecodings(String s) {
-         int[] dp = new int[s.length() + 1];
-        dp[0] = 1; // empty string
-        dp[1] = s.charAt(0) == '0' ? 0 : 1;
-        for (int i = 2; i < s.length() + 1; i++) {
-            if (s.charAt(i - 1) != '0') {
-                dp[i] += dp[i - 1];
-            }
-            if (
-                s.charAt(i - 2) == '1' ||
-                (s.charAt(i - 2) == '2' && s.charAt(i - 1) < '7')
-            ) {
-                dp[i] += dp[i - 2];
-            }
+        int n = s.length();
+        int dp[] = new int[n];
+
+        Arrays.fill(dp,-1);
+
+        return func(s,0,dp);
+    }
+
+    public int func(String s, int i, int dp[]){
+
+        if(i==s.length()){
+            return 1;
         }
-        return dp[s.length()];
+        if(s.charAt(i)=='0'){
+            return 0;
+        }
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        int count =0;
+        count+=func(s,i+1,dp);
+
+        if(i<s.length()-1 && (s.charAt(i)=='1' || s.charAt(i)=='2' && s.charAt(i+1)<'7' ) ){
+            count+=func(s,i+2,dp);
+        }
+        dp[i] = count;
+
+        return dp[i];
+
+
     }
 }
