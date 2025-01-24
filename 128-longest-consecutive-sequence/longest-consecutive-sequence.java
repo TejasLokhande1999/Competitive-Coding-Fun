13686196ed3ad6a2834b6c4ac65e36d13159d5cd
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2025-01-25 01:11:06 +0530

    Time: 33 ms (43.16%) | Memory: 66.6 MB (50.56%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,36 +1,38 @@
 class Solution {
-    int max = 0;
     public int longestConsecutive(int[] nums) {
+        int maxLength = 0;
+        Set<Integer> set = new HashSet<>();
 
-    Set<Integer> set = new HashSet<>();
-
-    for(int i=0;i<nums.length;i++){
-        set.add(nums[i]);
-    }
-
-    Iterator<Integer> itr = set.iterator();
-
-    while(itr.hasNext()){
-        int val = itr.next();
-        if(set.contains(val-1)){
-            //means that it's not the start of sequence
-            continue;
-        }else{
-            calculate(set,val);
+        for(int i=0;i<nums.length;i++){
+            set.add(nums[i]);
         }
+
+        Iterator<Integer> itr = set.iterator();
+
+        while(itr.hasNext()){
+            int ele = itr.next();
+            if(set.contains(ele-1)){
+                //then it is not the start
+                continue;
+            }else{
+                //it is the start. Cal the mas length
+                int count = calc(set,ele);
+                maxLength = Math.max(maxLength, count);
+
+            }
+        }
+
+        return maxLength;
     }
 
-    return max;
-
-    }
-
-    public void calculate(Set<Integer> set, int ele){
-        int count=0;
-
+    public int calc(Set<Integer> set, int ele){
+        int count =0;
         while(set.contains(ele)){
             count+=1;
-            max = Math.max(count,max);
             ele+=1;
         }
+
+        return count;
+
     }
 }
