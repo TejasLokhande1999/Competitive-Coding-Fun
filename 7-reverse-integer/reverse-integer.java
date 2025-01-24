7ed09b4a704b7bf3a2d0923744d179470a7681a7
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2025-01-24 23:03:29 +0530

    Time: 10 ms (3.79%) | Memory: 41.7 MB (5.84%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -1,46 +1,35 @@
 class Solution {
     public int reverse(int x) {
-        
+        boolean isNeg = false;
+
         String str = String.valueOf(x);
 
         if(str.charAt(0)=='-'){
-            char negArr[] = new char[str.length()-1];
-
-            for(int i=0;i<negArr.length;i++){
-                negArr[i] = str.charAt(i+1);
-            }
-
-            return -reverseString(negArr);
-
-        }else{
-
-            return reverseString(str.toCharArray());
-
+            isNeg = true;
+            str = str.substring(1,str.length());
         }
+
+        String retStr = "";
+
+        int n = str.length();
+        for(int i=0;i<n;i++){
+            retStr += str.charAt(n-i-1);
+        }   
+        
+
+        long retNum = 0;
+        retNum = Long.parseLong(retStr);
+        if(isNeg){
+            retNum = -retNum;
+        }
+
+        if(retNum>Integer.MAX_VALUE || retNum<Integer.MIN_VALUE){
+            return -0;
+        }
+
+        return (int) retNum;
+
+
 
     }
-
-
-    public int reverseString(char arr[]){
-
-        for(int i=0;i<arr.length/2;i++){
-            char temp = arr[i];
-            arr[i] = arr[arr.length-i-1];
-            arr[arr.length-i-1] = temp;
-        }
-
-        String retStr = String.valueOf(arr);
-
-        int retInt=0;
-
-        try{
-        retInt = Integer.parseInt(retStr);
-        }catch(NumberFormatException e){
-            return 0;
-        }
-
-        return retInt;
-    
-    }
-
 }
