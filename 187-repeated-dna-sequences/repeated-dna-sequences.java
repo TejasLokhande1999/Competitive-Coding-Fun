ed7eee98583fde8144b0f8cfa9a43c2690f96bde
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2025-01-25 12:42:13 +0530

    Time: 20 ms (51.64%) | Memory: 53.8 MB (29.62%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.java
+++ 187-repeated-dna-sequences/repeated-dna-sequences.java
@@ -1,20 +1,29 @@
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-        Map<String,Integer> map = new HashMap<>();
-        List<String> list = new ArrayList<>();
-        for(int i=0;i<=s.length()-10;i++){
-            String str= s.substring(i,i+10);
-            if(map.containsKey(str)){
-                if(map.get(str)==1){
-                    //now update
-                    list.add(str);
-                }
-                map.put(str,map.getOrDefault(str,0)+1);
+        int n= s.length();
+        Map<String, Boolean> map = new HashMap<>();
+        for(int i=0;i<=n-10;i++){
+            String str = s.substring(i,i+10);
+            if(!map.containsKey(str)){
+                map.put(str,false);
             }else{
-                map.put(str,1);
-            }
+                map.put(str,true);
+            }            
 
         }
+
+        List<String> list = new ArrayList<>();
+
+        for(Map.Entry<String,Boolean> entry : map.entrySet()){
+            boolean flag = entry.getValue();
+            String key = entry.getKey();
+            if(flag){
+                list.add(key);
+            }
+        }
+
         return list;
+
+
     }
 }
