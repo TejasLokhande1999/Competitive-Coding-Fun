6d2b90ebd64f920daacce6ef209140fee2dfa986
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2025-01-27 12:37:18 +0530

    Time: 1 ms (70.62%) | Memory: 41.6 MB (65.10%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.java
+++ 165-compare-version-numbers/compare-version-numbers.java
@@ -1,19 +1,26 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
-                    String[] s1 = version1.split("\\."); // split the version on . character
-            String[] s2 = version2.split("\\.");
+        
+        String str1[] = version1.split("\\.");
+        String str2[] = version2.split("\\.");
+        int s1Lgt = str1.length;
+        int s2Lgt = str2.length;
+        int n = Math.max(str1.length,str2.length);
 
-            int maxLength = Math.max(s1.length, s2.length);
-            for (int i = 0; i < maxLength; i++) {  // loop for max length
-                int v1 = i < s1.length ? Integer.parseInt(s1[i]) : 0; // check if s1 still have level then parsetInt or else 0
-                int v2 = i < s2.length ? Integer.parseInt(s2[i]) : 0; // check if s2 still have level then parsetInt or else 0
-                if (v1 > v2) // just compare on each level whenever any condition true return in case of v1 == v2 continue
-                    return 1;
-                else if (v1 < v2)
-                    return -1;
+        for(int i=0;i<n;i++){
+
+            int num1 = s1Lgt<= i ? 0 : Integer.parseInt(str1[i]);
+            int num2 = s2Lgt<= i ? 0: Integer.parseInt(str2[i]);
+
+            if(num1<num2){
+                return -1;
+            }else if(num1>num2){
+                return 1;
             }
 
-            return 0;
+        }
+
+        return 0;
 
     }
 }
