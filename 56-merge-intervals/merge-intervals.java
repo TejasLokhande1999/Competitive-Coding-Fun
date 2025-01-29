8e1b74c9c4b83d2213f98c24df2c0b9d9d86fde2
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2025-01-30 00:33:18 +0530

    Time: 8 ms (87.05%) | Memory: 46.8 MB (25.57%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,75 +1,39 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         
-        if(intervals.length==1){
-            return intervals;
+        //sort the intervals according to start time
+
+        Arrays.sort(intervals, (a,b) -> a[0]- b[0]);
+
+        List<int[]> list = new ArrayList<>();
+        //sort based on start time
+
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+
+        for(int i=1;i<intervals.length;i++){
+
+            int currStart = intervals[i][0];
+            int currEnd  = intervals[i][1];
+
+            if(currStart<=end){
+                start = Math.min(start,currStart);
+                end = Math.max(end, currEnd);
+            }else{
+                
+                list.add(new int[]{start,end});
+                start = currStart;
+                end = currEnd;
+
         }
-        ArrayList<Pair> list = new ArrayList<>();
-
-        for(int i=0;i<intervals.length;i++){
-            int x = intervals[i][0];
-            int y = intervals[i][1];
-
-            list.add(new Pair(x,y));
         }
 
-    Collections.sort(list, new PairComparator());
+        list.add(new int[]{start,end});
+
+        return list.toArray(new int[list.size()][]);
+
+        
 
 
-    ArrayList<Pair> retList = new ArrayList<Pair>();
-
-    Pair p1 = list.get(0);
-    int ele1 = p1.x;
-    int ele2=p1.y;
-    for(int i=1;i<list.size();i++){
-        Pair p = list.get(i);
-        if(p.x<=ele2){
-
-            if(p.y>ele2){
-                ele2=p.y;
-            }
-
-        }else{  //start a new interval
-            retList.add(new Pair(ele1,ele2));
-            ele1 = p.x;
-            ele2 = p.y;
-        }
     }
-
-    retList.add(new Pair(ele1,ele2));
-
-    int mat[][] = new int[retList.size()][2];
-
-    for(int i=0;i<retList.size();i++){
-        mat[i][0] = retList.get(i).x;
-        mat[i][1] = retList.get(i).y;
-    }
-
-    return mat;
-}
-}
-
-class Pair{
-
-    int x;
-    int y;
-    Pair(int x, int y){
-        this.x = x;
-        this.y=y;
-    }
-}
-
-class PairComparator implements Comparator<Pair>{
-
-    public int compare(Pair p1, Pair p2){
-
-        if(p1.x>p2.x){
-            return 1;
-        }else if(p1.x<p2.x){
-            return -1;
-        }else{          //p1.x==p2.x;
-            return 0;
-        }
-    }
-
 }
