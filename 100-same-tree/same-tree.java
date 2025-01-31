d8cb9289ed470cb95912406a979aac153dc87040
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2025-01-31 07:53:13 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (52.75%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -19,15 +19,17 @@ class Solution {
             return true;
         }
 
+
+        //if either of them are null
         if(p==null || q==null){
             return false;
         }
 
-        if(p.val == q.val){
-            
-            return isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
+        if(p.val==q.val){
 
+            return isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
         }
+
         return false;
     }
 }
