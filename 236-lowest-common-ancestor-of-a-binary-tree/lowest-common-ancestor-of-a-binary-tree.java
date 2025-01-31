50ecd2483f28e6224dd2e2848ac6d4dfa656c34f
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2025-02-01 01:55:21 +0530

    Time: 7 ms (64.63%) | Memory: 44.9 MB (47.29%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -9,38 +9,29 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        List<TreeNode> listP = new ArrayList<>();
-        List<TreeNode> listQ = new ArrayList<>();
-
-        inorder(root,p,listP);
-        inorder(root,q,listQ);
-
-        int n = listP.size()>listQ.size()?listQ.size():listP.size();
-
-        for(int i=0;i<n;i++){
-            if(listP.get(i)!=listQ.get(i)){
-                return listP.get(i-1);
-            }
+        if(root==null){
+            return null;
         }
 
-        return listP.get(n - 1);
-    }
 
-    public boolean inorder(TreeNode node, TreeNode ele, List<TreeNode> list){
-        if(node==null){
-            return false;
+        if(p.equals(root)||q.equals(root)){
+            return root;
         }
 
-        list.add(node);
-        if(node.equals(ele)){
-            return true;
+        TreeNode left = lowestCommonAncestor(root.left,p,q);
+        TreeNode right = lowestCommonAncestor(root.right,p,q);
+
+        if(left==null){
+            return right;
+        }else if(right==null){
+            return left;
+        }else{
+            //both the nodes has some val so that's the LCA
+
+
+            return root;
         }
 
-        if(inorder(node.left,ele,list)||inorder(node.right,ele,list)){
-            return true;
-        }
 
-        list.remove(list.size()-1);
-        return false;
     }
 }
