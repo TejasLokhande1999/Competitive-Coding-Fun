b517aad77078bfcc6c6c0e10e3361a9e447139cd
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2025-02-03 02:32:37 +0530

    Time: 4 ms (27.92%) | Memory: 45.5 MB (86.99%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,48 +1,41 @@
 class Solution {
     public int candy(int[] ratings) {
-        int n = ratings.length;
-        int nums[] = new int[n];
 
-        Arrays.fill(nums,1);
+        int n = ratings.length;
+        int candies[] = new int[n];
+
+
+        Arrays.fill(candies,1);
+
 
         if(n==1){
             return 1;
         }
+
+
         for(int i=1;i<n;i++){
             if(ratings[i]>ratings[i-1]){
-                nums[i] = nums[i-1]+1;
+                candies[i] = 1+candies[i-1];
             }
         }
 
+
         for(int i=n-2;i>=0;i--){
+
             if(ratings[i]>ratings[i+1]){
-                if(nums[i]<=nums[i+1]){
-                    nums[i] = nums[i+1]+1;
-                }
+                if(candies[i]<=candies[i+1])
+                    candies[i] = 1 + candies[i+1];
             }
+
         }
 
         int sum = 0;
-        for(Integer i : nums){
+        for(Integer i : candies){
             sum+=i;
         }
 
         return sum;
-    }
+
+
+        }
 }
-
-
-    //  1 2 3 4 5
-    //  1 2 3 4 5
-
-    //  5 4 3 2 1
-    //  5 4 3 2 1
-
-    //  1 2 3 4 5 2 1 2 3
-    //  1 2 3 4 5 3 2 4 5
-
-        // 1 1 2
-        // 1 0 2
-
-        // 1 1 1. 1. 1. 1 1  
-        // 1 2 87 87 87 2 1   
