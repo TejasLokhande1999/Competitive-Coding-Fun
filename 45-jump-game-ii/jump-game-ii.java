b902d9dc31a02cdfeb3a632364b8cfd44b9fd26f
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2025-02-03 00:31:08 +0530

    Time: 1 ms (99.45%) | Memory: 44.8 MB (92.78%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,28 +1,23 @@
 class Solution {
     public int jump(int[] nums) {
-        
         int n = nums.length;
 
-        int res = 0;
-
         int l =0;
-        int r=0;
-
+        int r =0;
+        int res=0;
         while(r<n-1){
-            int maxWidth = 0;
+
+            res++;
+            int endpoint = 0;
             for(int i=l;i<=r;i++){
-                maxWidth = Math.max(i+nums[i],maxWidth);
+                endpoint = Math.max(endpoint,i + nums[i]);
             }
 
-            res+=1;
             l = r+1;
-            r = maxWidth;
-
+            r = endpoint;
         }
 
+
         return res;
-
     }
-
-
 }
