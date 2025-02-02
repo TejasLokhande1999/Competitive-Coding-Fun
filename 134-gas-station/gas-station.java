1bab74bd1deed947869d11231c3487583d5b1cfe
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2025-02-02 07:39:05 +0530

    Time: 2 ms (97.73%) | Memory: 56.5 MB (23.41%) - LeetSync

--- 134-gas-station/gas-station.java
+++ 134-gas-station/gas-station.java
@@ -1,27 +1,27 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int totalGas = 0;  // Total gas surplus/deficit
-        int currentGas = 0;  // Current surplus at this point
-        int startIndex = 0;  // Starting point
-        
-        for (int i = 0; i < gas.length; i++) {
-            int gasDiff = gas[i] - cost[i];  // Net gas gain at this station
-            totalGas += gasDiff;  // Add to the total gas difference
-            currentGas += gasDiff;  // Add to the current gas balance
-            
-            // If currentGas drops below 0, reset the starting point to the next station
-            if (currentGas < 0) {
-                startIndex = i + 1;
-                currentGas = 0;  // Reset current gas as we're starting from a new point
-            }
+        int sumGas = 0;
+        int costSum = 0;
+        int n = gas.length;
+        for(int i=0;i<n;i++){
+            sumGas+=gas[i];
+            costSum+=cost[i];
         }
-        
-        // If the total gas is negative, no solution exists
-        if (totalGas < 0) {
+
+        if(sumGas<costSum){
             return -1;
         }
-        
-        // Otherwise, return the valid starting index
-        return startIndex;
+
+        int start = 0;
+        int currGas = 0;
+        for(int i=0;i<n;i++){
+            currGas += gas[i]-cost[i];
+            if(currGas<0){
+                currGas = 0;
+                start = i+1;
+            }
+        }
+
+        return start;
     }
 }
