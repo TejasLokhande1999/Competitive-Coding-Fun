7a6c8e545fa7e411b843e182af13ad5753fbd69a
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2025-02-03 19:15:08 +0530

    Time: 20 ms (51.87%) | Memory: 44.7 MB (55.79%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,42 +1,46 @@
 class Solution {
     public String longestPalindrome(String s) {
+        String subStr = "";
         int n = s.length();
-        String res = "";
-        int len =0;
-
+        int maxLen = 0;
         for(int i=0;i<s.length();i++){
-            int l,r;
-            
 
-                l=i;
-                r=i;
+            //for odd
+            int l = i;
+            int r = i;
+            while(l>=0 && r<n){
 
-
-            while(l>=0 && r<n && s.charAt(l)==s.charAt(r)){
-                
-                if(r-l+1>len){
-                    len = r-l+1;
-                    res = s.substring(l,r+1);
+                if(s.charAt(l)==s.charAt(r)){
+                    if(r-l+1>maxLen){
+                        subStr = s.substring(l,r+1);
+                        maxLen = r-l+1;
+                    }
+                    l--;
+                    r++;
+                }else{
+                    break;
                 }
 
+            }
+
+
+
+            //for evem
+            l = i;
+            r = i + 1;
+            while (l >= 0 && r < s.length() && 
+                   s.charAt(l) == s.charAt(r)) {
+                if (r - l + 1 > maxLen) {
+                    subStr = s.substring(l,r+1);
+                    maxLen = r - l + 1;
+                }
                 l--;
                 r++;
             }
-
-               l = i;
-                r=l+1;
-             while(l>=0 && r<n && s.charAt(l)==s.charAt(r)){
-                
-                if(r-l+1>len){
-                    len = r-l+1;
-                    res = s.substring(l,r+1);
-                }
-
-                l--;
-                r++;
-            }
-
         }
-        return res;
+
+        return subStr;
+
+
     }
 }
