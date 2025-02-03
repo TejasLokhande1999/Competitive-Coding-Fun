1bbdc921302bd4e24631127b149b813554f38728
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2025-02-03 07:44:25 +0530

    Time: 14 ms (57.07%) | Memory: 49.1 MB (25.51%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,44 +1,66 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
+        
         Map<Integer, Integer> map = new HashMap<>();
 
         for(int i=0;i<nums.length;i++){
             map.put(nums[i], map.getOrDefault(nums[i],0)+1);
         }
-    
-    Queue<Pair> pq = new PriorityQueue<>((a,b)-> a.freq - b.freq);
 
-    for(Map.Entry<Integer, Integer>entry : map.entrySet()){
-        int key = entry.getKey();
-        int freq = entry.getValue();
+        PriorityQueue<Pair> pq = new PriorityQueue<>(new PairComparator());
 
-        pq.add(new Pair(key,freq));
-        if(pq.size()>k){
-            pq.poll();
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+
+            int key = entry.getKey();
+            int freq = entry.getValue();
+
+            pq.add(new Pair(key,freq));
+
+            if(pq.size()>k){
+                pq.poll();
+            }
+
         }
 
-    }
-
-    int retArr[] = new int[pq.size()];
-
-    int i=0;
-    while(!pq.isEmpty()){
-        retArr[i] = pq.poll().ele;
+        int retArr[] = new int[pq.size()];
+        int i=0;
+        while(!pq.isEmpty()){
+        retArr[i] = pq.poll().key;
         i++;
-    }
+        }
 
-    return retArr;
-
-
+        return retArr;
     }
 }
 
 class Pair{
-    int ele;
+
+    int key;
     int freq;
-    Pair(int ele, int freq){
-        this.ele = ele;
+
+    Pair(int key , int freq){
+        this.key = key;
         this.freq = freq;
     }
+
 }
 
+
+
+//sort in ascending order
+class PairComparator implements Comparator<Pair>
+{
+
+    public int compare(Pair p1, Pair p2){
+
+        if(p1.freq>p2.freq){
+            return 1;
+        }else if(p1.freq<p2.freq){
+            return -1;
+        }else{
+            return 0;
+        }
+
+    }
+
+}
