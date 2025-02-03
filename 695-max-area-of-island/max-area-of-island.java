ecb9502d2fce25c951672f1b99d77d96af4a1e52
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2025-02-03 20:12:27 +0530

    Time: 2 ms (70.15%) | Memory: 44.6 MB (60.56%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,42 +1,45 @@
 class Solution {
     public int maxAreaOfIsland(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
+        int n = grid.length;
+        int m = grid[0].length;
+
+
         int max = 0;
-        int count[] = new int[1];
-        boolean visited[][] = new boolean[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==1 &&!visited[i][j]){
-                    count[0]=0;
-                    dfs(i,j,grid,visited,count);
-                    max=Math.max(count[0],max);
-                }
+        boolean visited[][] =new boolean[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+
+                    if(grid[i][j]==1 && !visited[i][j]){
+                        int area = dfs(i,j,grid,visited);
+                        max = Math.max(max,area);
+                    }
+
             }
         }
 
         return max;
     }
 
-        public void dfs(int row, int col,int grid[][], boolean visited[][], int count[]){
 
-            int m = grid.length;
-            int n = grid[0].length;
+    public int dfs(int row, int col, int[][] grid, boolean[][] visited){
 
-            if(row<0 || col<0 || row>=m || col>=n || visited[row][col] || grid[row][col]==0){
-                return;
-            }
+        int n = grid.length;
+        int m = grid[0].length;
 
-                visited[row][col] = true;
-                count[0]+=1;
-
-
-            dfs(row+1,col,grid,visited,count);
-            dfs(row-1,col,grid,visited,count);
-            dfs(row,col+1,grid,visited,count);
-            dfs(row,col-1,grid,visited,count);
-
-            
+        if(row<0 || row>=n || col<0 || col>=m || visited[row][col]==true || grid[row][col]==0){
+            return 0;
         }
-    
+
+        visited[row][col] = true;
+
+        int val = 1;
+
+        val+=dfs(row+1,col,grid,visited);
+        val+=dfs(row-1,col,grid,visited);
+       val+= dfs(row,col+1,grid,visited);
+       val+= dfs(row,col-1,grid,visited);
+
+        return val;
+
+    }
 }
