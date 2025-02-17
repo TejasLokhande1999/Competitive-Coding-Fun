fd030f64056b200b1c96eba090fb8a7f7decf3ba
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2025-02-17 11:18:49 +0530

    Time: 1 ms (23.00%) | Memory: 42.1 MB (29.41%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,34 +1,52 @@
 class Solution {
     public void sortColors(int[] nums) {
-       // Arrays.sort(nums);
-
-        int count0=0;
-        int count1=0;
-        int count2=0;
+        
+        int zero =0;
+        int one = 0;
+        int two=0;
 
         for(int i=0;i<nums.length;i++){
-            if(nums[i]==0){
-                count0+=1;
-            }else if(nums[i]==1){
-                count1+=1;
-            }else{
-                count2+=1;
+
+            if(nums[i]!=0){
+                //find the first zero
+
+                for(int j=i;j<nums.length;j++){
+                    if(nums[j]==0){
+                        //swap
+                        int temp = nums[j];
+                        nums[j] = nums[i];
+                        nums[i] = temp;
+                        break;
+                    }
+                }
+            }
+
+        }
+
+        int zeroIndex = 0;
+
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                zeroIndex=i;
+                break;
             }
         }
 
+        for(int i=zeroIndex;i<nums.length;i++){
+             if(nums[i]!=1){
+                //find the first zero
 
-        for(int i=0;i<count0;i++){
-            nums[i] = 0;
+                for(int j=i;j<nums.length;j++){
+                    if(nums[j]==1){
+                        //swap
+                        int temp = nums[j];
+                        nums[j] = nums[i];
+                        nums[i] = temp;
+                        break;
+                    }
+                }
+            }
+
         }
-
-        for(int i=count0;i<count0+count1;i++){
-            nums[i]=1;
-        }
-
-        for(int i=count1+count0;i<nums.length;i++){
-            nums[i]=2;
-        }
-
-
     }
 }
