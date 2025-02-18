dc49613c42efac56c7955627bca048c44cbc774b
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2025-02-19 00:38:23 +0530

    Time: 49 ms (30.25%) | Memory: 47.8 MB (50.62%) - LeetSync

--- 1397-search-suggestions-system/search-suggestions-system.java
+++ 1397-search-suggestions-system/search-suggestions-system.java
@@ -1,31 +1,60 @@
 class Solution {
-    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
-        PriorityQueue<String> pq = new PriorityQueue<>(Collections.reverseOrder());
-        List<List<String>> retList = new ArrayList<>();
-    for(int i=1;i<=searchWord.length();i++)
-    {
-        String childStr = searchWord.substring(0,i);
+
+    static class Node{
+        Map<Character,Node> map = new HashMap<>();
+        boolean isEndOfWord = false;
         List<String> list = new ArrayList<>();
-        for(String product : products){
-            if(product.startsWith(childStr))
-            {
-                pq.add(product);
-                if(pq.size()>3){
-                    pq.poll();
-                }
-            }
-        }
-
-        while(!pq.isEmpty()){
-            list.addFirst(pq.poll());
-        }
-            retList.add(list);
-
     }
+    Node root = new Node();
+    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+        List<List<String>> retList = new ArrayList<>();
+        Arrays.sort(products);
+        for(String product : products){
+            insert(product);
+        }
+
+        String pre="";
+        for(char ch : searchWord.toCharArray()){
+            pre+=ch;
+            retList.add(search(pre));
+        }
 
         return retList;
 
-
     }
 
+    public List<String> search(String pre){
+
+        Node curr = root;
+
+        for(char ch : pre.toCharArray()){
+
+            if(!curr.map.containsKey(ch)){
+                return new ArrayList<>();
+            }
+            curr =curr.map.get(ch);
+        }
+
+        return curr.list;
+    }
+
+    public void insert(String word){
+
+        Node currNode = root;
+
+        for(char ch : word.toCharArray()){
+
+            if(!currNode.map.containsKey(ch)){
+                currNode.map.put(ch,new Node());
+                
+            }
+            currNode = currNode.map.get(ch);
+             if(currNode.list.size()<3){
+                    currNode.list.add(word);
+                }
+        }
+
+        currNode.isEndOfWord = true;
+
+    }
 }
