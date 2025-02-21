c58ee50abecbafa2c2ec714c8b0796709b2f42d6
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2025-02-22 02:27:30 +0530

    Time: 8 ms (87.14%) | Memory: 46.9 MB (25.05%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,39 +1,40 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         
-        //sort the intervals according to start time
+        if(intervals.length==1){
+            return intervals;
+        }
 
-        Arrays.sort(intervals, (a,b) -> a[0]- b[0]);
+        //sorting based on start in asc
+        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
 
-        List<int[]> list = new ArrayList<>();
-        //sort based on start time
+        List<int[]> list= new ArrayList<>();
 
-        int start = intervals[0][0];
-        int end = intervals[0][1];
+        
+        int prevStart = intervals[0][0];
+        int prevEnd = intervals[0][1];
+        int start = 0;
+        int end = 0;
 
         for(int i=1;i<intervals.length;i++){
 
-            int currStart = intervals[i][0];
-            int currEnd  = intervals[i][1];
+            start = intervals[i][0];
+            end = intervals[i][1];
 
-            if(currStart<=end){
-                start = Math.min(start,currStart);
-                end = Math.max(end, currEnd);
+
+            if(start<=prevEnd){
+                prevStart = Math.min(prevStart,start);
+                prevEnd = Math.max(prevEnd,end);
             }else{
-                
-                list.add(new int[]{start,end});
-                start = currStart;
-                end = currEnd;
-
+                list.add(new int[]{prevStart,prevEnd});
+                prevStart = start;
+                prevEnd = end;
+            }
         }
-        }
-
-        list.add(new int[]{start,end});
+        
+        list.add(new int[]{prevStart,prevEnd});
 
         return list.toArray(new int[list.size()][]);
-
-        
-
 
     }
 }
