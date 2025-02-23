c10ccc131660e7f93900fcb6bd5ab2d4b3f9fc16
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2025-02-23 10:07:06 +0530

    Time: 1 ms (88.46%) | Memory: 47.8 MB (56.97%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,56 +1,33 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        //change to adj matrix;
-
+        
         int V = isConnected.length;
 
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        boolean visited[] = new boolean[V];
+        V = V;
+        int count=0;
 
-
-        for(int i=0;i<V+1;i++){
-            adj.add(new ArrayList<>());
-        }
-        int n = isConnected.length;
-        int m = isConnected[0].length;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(isConnected[i][j]==1){
-                    adj.get(i).add(j);
-                    adj.get(j).add(i);
-                }
-            }
-        }
-        int count = 0;
-
-        boolean visited[] = new boolean[V+1];
         for(int i=0;i<V;i++){
-            
+
             if(!visited[i]){
-            count+=1;
-            dfs(i,visited,adj);
-
+                count+=1;
+                dfs(i,isConnected, visited);
             }
-
         }
 
         return count;
 
     }
 
-    public void dfs(int currNode, boolean[] visited, ArrayList<ArrayList<Integer>> adj){
+    public void dfs(int currNode, int[][] grid, boolean visited[]){
 
         visited[currNode] = true;
 
-        ArrayList<Integer> list = adj.get(currNode);
-
-        for(int i=0;i<list.size();i++){
-
-            int node = list.get(i);
-            if(!visited[node]){
-                dfs(node,visited,adj);
+        for(int i=0;i<grid[0].length;i++){
+            int node = grid[currNode][i];
+            if(!visited[i] && node==1){
+                dfs(i,grid,visited);
             }
-
         }
-
     }
 }
