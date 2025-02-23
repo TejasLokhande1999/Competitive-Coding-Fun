ce24e10a98efeb4c7652fa3c6c5ce2519db83ff0
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2025-02-23 11:24:07 +0530

    Time: 2 ms (70.92%) | Memory: 42.9 MB (21.58%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,75 +1,88 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
         
-        Queue<Pair> q = new LinkedList<>();
         int m = grid.length;
         int n = grid[0].length;
 
         boolean visited[][] = new boolean[m][n];
+
+        Queue<Pair> q = new LinkedList<>();
+
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==2){
+                    //these are the rotten oranges
                     q.add(new Pair(i,j,0));
-                    visited[i][j]=true;
+                    visited[i][j] = true;
                 }
             }
         }
 
-        int tmax=0;
-
-        while(!q.isEmpty()){
-            Pair p = q.poll();
-            int row =p.row;
-            int col =p.col;
-            int time = p.time;
-
-            tmax= Math.max(tmax,time);
-
-            int nRow[] = {-1,0,1,0};
-            int nCol[] = {0,1,0,-1};
-
-            for(int k=0;k<4;k++){
-                int newRow = row+nRow[k];
-                int newCol = col+nCol[k];
-
-                //check the condition
-                if(newRow>=0 && newRow<m && newCol>=0 && newCol<n && !visited[newRow][newCol] && grid[newRow][newCol]==1){
-                    //then visit
-                    //we are visiting the unvisited 1 node
-                    q.add(new Pair(newRow, newCol, time+1));
-                    visited[newRow][newCol]=true;
-                }
-            }
-
-        }
-
-
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==1 && visited[i][j]==false){
-                    return -1;
-                }
-            }
-        }
-
-        return tmax;
-
-
-
+       return  bfs(q,visited,grid);
 
     }
+
+        public int bfs(Queue<Pair> q, boolean visited[][], int grid[][]){
+        int globalTime = 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        while(!q.isEmpty()){
+
+            Pair p = q.poll();
+            int row = p.row;
+            int col = p.col;
+            int time = p.time;
+            globalTime = Math.max(globalTime,time);
+            //trace the 4 directions 
+
+            //1,0  -1,0  0,-1  0,1
+            int rArr[] = {1,-1,0,0};
+            int cArr[] = {0,0,-1,1};
+            
+            for(int k=0;k<4;k++){
+
+                int nRow = rArr[k]+row;
+                int nCol = cArr[k]+col;
+
+
+                //check if within bounds
+
+                if(nRow>=0 && nRow<m && nCol>=0 && nCol<n && !visited[nRow][nCol]
+                && grid[nRow][nCol]==1){
+                    q.add(new Pair(nRow,nCol,time+1));
+                    visited[nRow][nCol]=true;
+                }
+
+            }
+        }
+
+        //
+
+            for(int i=0;i<m;i++){
+                for(int j=0;j<n;j++){
+
+                    if(grid[i][j]==1 && !visited[i][j]){
+                        return -1;
+                    }
+
+                }
+            }
+
+
+            return globalTime;
+
+
+        }
 }
 
 class Pair{
-
     int row;
     int col;
     int time;
 
     Pair(int row, int col, int time){
         this.row =row;
-        this.col=col;
+        this.col = col;
         this.time = time;
     }
-
 }
