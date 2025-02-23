901aeda170151402719a56e7df5326f2fa795371
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2025-02-23 10:15:57 +0530

    Time: 4 ms (51.20%) | Memory: 49.6 MB (60.51%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,36 +1,41 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        boolean visited[][] = new boolean[m][n];
-        int counter=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]=='1' && !visited[i][j]){
-                    dfs(i,j,visited,grid);
-                    counter++;
+        
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        boolean visited[][]= new boolean[rows][cols];
+        int count=0;
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                
+                if(!visited[i][j] && grid[i][j]=='1'){
+                    count+=1;
+                    dfs(i,j,grid,visited);
                 }
+
             }
         }
 
-        return counter;
+        return count;
+
+
     }
 
-    public void dfs(int row, int col, boolean visited[][], char grid[][]){
 
-        int m = grid.length;
-        int n = grid[0].length;
-
-        if(row<0 || row>=m || col<0 || col>=n || grid[row][col]=='0' || visited[row][col]){         //already visited
+    public void dfs(int row, int col, char[][] grid, boolean visited[][]){
+        int n = grid.length;
+        int m = grid[0].length;
+        if(row>=n || row<0 || col>=m || col<0 || visited[row][col] || grid[row][col]=='0'){
             return;
         }
 
-        visited[row][col]=true;
+        visited[row][col] = true;
 
-        dfs(row+1,col,visited,grid);
-        dfs(row-1,col,visited,grid);
-        dfs(row,col+1,visited,grid);
-        dfs(row,col-1,visited,grid);
+        dfs(row-1,col,grid,visited);
+        dfs(row+1,col,grid,visited);
+        dfs(row,col-1,grid,visited);
+        dfs(row,col+1,grid,visited);
 
     }
 }
