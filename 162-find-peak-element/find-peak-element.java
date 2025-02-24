91a14fa36f5907a0d033ea2bc59f1bc2675fe721
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2025-02-24 10:33:52 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (93.84%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,93 +1,36 @@
 class Solution {
-    public int findPeakElement(int[] arr) {
+    public int findPeakElement(int[] nums) {
         
-        int n = arr.length;
+        int n = nums.length;
 
-        if(arr.length==1){
+        if(nums.length==1){
+            return 0;
+        }
+        if(nums[0]>nums[1]){
             return 0;
         }
 
-        if(arr[0]>arr[1]){
-            return 0;
-        }
-
-        if(arr[n-1]>arr[n-2]){
+        if(nums[n-1]>nums[n-2]){
             return n-1;
-        }else{
-
-            int start = 1;
-            int end = n-2;
-
-            while(start<=end){
-
-                int mid = start + (end-start)/2;
-
-                if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
-                    return mid;
-                }else if(arr[mid]<arr[mid+1]){
-
-                        start =mid+1;
-
-                }else{
-                    end = mid-1;
-
-                }
-
-
-            }
-
-            return -1;
-
-
         }
 
+        int start = 1;
+        int end = n-2;
 
+        while(start<=end){
 
+            int mid = start+(end-start)/2;
 
+            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
+                return mid;
+            }else if(nums[mid]<nums[mid+1]){
+                start = mid+1;  //move to right as prob of finding peak here is nore
+            }else{
+                end = mid-1;
+            }
+        }
 
-
-    //     if(arr.length==1){
-    //         return 0;
-    //     }
-
-    //     if(arr[0]>arr[1]){
-    //         return 0;
-    //     }
-
-    //     int n = arr.length;
-
-    //     if(arr[n-1]>arr[n-2]){
-    //         return n-1;
-    //     }
-
-    //     // for(int i=1;i<n-1;i++){
-    //     //     if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
-    //     //         return i;
-    //     //     }
-    //     // }
-
-    //     // return -1;
-
-    //     int start =1;
-    //     int end =n-2;
-
-    //     while(start<=end){
-    //         int mid = start + (end-start)/2;
-
-    //         if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
-    //             return mid;
-    //         }else if(arr[mid-1]< arr[mid] && arr[mid]<arr[mid+1]){
-    //             //peak element at right
-
-    //             start = mid+1;
-    //         }else{
-    //             end = mid-1;
-    //         }
-
-    //     }
-
-
-    // return -1;
+        return -1;
 
     }
 }
