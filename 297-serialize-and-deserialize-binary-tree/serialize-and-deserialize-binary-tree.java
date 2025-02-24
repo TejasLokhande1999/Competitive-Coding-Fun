38669994cd78ffd034845f12756eef2bf5eafcb1
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2025-02-24 21:59:46 +0530

    Time: 19 ms (37.31%) | Memory: 46.4 MB (30.32%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
@@ -11,20 +11,15 @@ public class Codec {
 
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        StringBuilder str = new StringBuilder();
-
-
         if(root==null){
             return "";
         }
 
-        Queue<TreeNode> q = new LinkedList<>();
-
+        Queue<TreeNode> q= new LinkedList<>();
+        StringBuilder str = new StringBuilder();
         q.add(root);
-
         while(!q.isEmpty()){
-            TreeNode node =q.poll();
-
+            TreeNode node = q.poll();
             if(node==null){
                 str.append("# ");
                 continue;
@@ -33,44 +28,40 @@ public class Codec {
             }
             q.add(node.left);
             q.add(node.right);
-
-        }
-
+    }
         return str.toString();
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        
         if(data==""){
             return null;
         }
-
         String strs[] = data.split(" ");
-
-        TreeNode root = new TreeNode(Integer.parseInt(strs[0]));
-        Queue<TreeNode> q = new LinkedList<>();
+        TreeNode root= new TreeNode(Integer.parseInt(strs[0]));
+        Queue<TreeNode> q= new LinkedList<>();
         q.add(root);
+
         for(int i=1;i<strs.length;i++){
-        TreeNode parent = q.poll();
+
+            TreeNode parent = q.poll();
 
             if(!strs[i].equals("#")){
                 TreeNode left = new TreeNode(Integer.parseInt(strs[i]));
-                parent.left =left;
+                parent.left = left;
                 q.add(left);
-
             }
 
             if(!strs[++i].equals("#")){
                 TreeNode right = new TreeNode(Integer.parseInt(strs[i]));
-                parent.right =right;
+                parent.right = right;
                 q.add(right);
-
             }
 
         }
-
         return root;
+        
+
     }
 }
 
