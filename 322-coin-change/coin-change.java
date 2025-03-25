ef716a19ef33dc11ab6cc1db3c9dbe7ec18d84c7
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2025-03-25 11:30:32 +0530

    Time: 25 ms (33.39%) | Memory: 45.1 MB (32.12%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,39 +1,38 @@
 class Solution {
+    int dp[];
     public int coinChange(int[] coins, int amount) {
-        int dp[] = new int[amount+1];
-
+        dp = new int[amount+1];
         Arrays.fill(dp,-1);
+        int res =  dfs(coins,amount);
 
-        int result = dfs(coins,amount,dp);
-
-        return result == Integer.MAX_VALUE ? -1 : result;
+        if(res>=Integer.MAX_VALUE){
+            return -1;
+        }
+        return res;
     }
 
-    public int dfs(int coins[], int amount, int dp[]){
+    public int dfs(int coins[], int amount){
 
         if(amount==0){
             return 0;
         }
-        if(amount<0){
-            return Integer.MAX_VALUE;
-        }
         if(dp[amount]!=-1){
-            return dp[amount];      //how many min coins are needed for this amount
+            return dp[amount];
+        }
+        
+        int minCoins = Integer.MAX_VALUE;
+        for(int coin : coins){
+                
+                if(amount-coin>=0){
+                    int res = dfs(coins,amount-coin);
+                    if(res!=Integer.MAX_VALUE){
+                        minCoins = Math.min(minCoins,1+res);
+                    }
+                }
         }
 
-        int minCoins =Integer.MAX_VALUE;
 
-        for(Integer coin : coins){
-
-            int ans = dfs(coins, amount-coin,dp);
-
-            if(ans!=Integer.MAX_VALUE)
-                minCoins = Math.min(minCoins,ans+1);
-        }
-
-        dp[amount] = minCoins;
-
-        return minCoins;
+        return dp[amount] =  minCoins;
 
     }
 }
