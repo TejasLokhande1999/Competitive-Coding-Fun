5459b6a0dcc95cb6848208419044e3e43ad34553
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2025-03-25 06:29:24 +0530

    Time: 19 ms (58.92%) | Memory: 45.3 MB (36.40%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,56 +1,38 @@
 class Solution {
     public String longestPalindrome(String s) {
         
-        int maxLen = Integer.MIN_VALUE;
-        String retstr = "";
+        int maxLen = 0;
+        String maxStr="";
         int n = s.length();
-
+        int l = 0;
+        int r = 0;
         for(int i=0;i<n;i++){
-
-            int l = i;
-            int r = i;
-
-            while(l>=0 && r<n){
-
-                if(s.charAt(l)==s.charAt(r)){
-                    
-                    if(r-l+1>maxLen){
-                        maxLen = r-l+1;
-                        retstr = s.substring(l,r+1);
-                    }
-                    l--;
-                    r++;
-                }else{
-                    break;
+            l = i;
+            r = i;
+            while(l>=0 && r<n && s.charAt(l) == s.charAt(r)){
+                if(r-l+1>maxLen){
+                    maxLen = Math.max(maxLen,r-l+1);
+                    maxStr = s.substring(l,r+1);
                 }
-
-
+                l--;
+                r++;
             }
 
-            l=i;
-            r=i+1;
+            //for even length
+            l = i;
+            r = i+1;
 
-            while(l>=0 && r<n){
-
-                if(s.charAt(l)==s.charAt(r)){
-                    
-                    if(r-l+1>maxLen){
-                        maxLen = r-l+1;
-                        retstr = s.substring(l,r+1);
-                    }
-                    l--;
-                    r++;
-                }else{
-                    break;
+            while(l>=0 && r<n && s.charAt(l) == s.charAt(r)){
+                if(r-l+1>maxLen){
+                    maxLen = Math.max(maxLen,r-l+1);
+                    maxStr = s.substring(l,r+1);
                 }
-
-
+                l--;
+                r++;
             }
-
 
         }
 
-        return retstr;
-
+        return maxStr;
     }
 }
