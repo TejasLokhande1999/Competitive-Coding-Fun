2d5e8b72353ca5077faf6828a950a9da63829781
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2025-03-26 03:54:30 +0530

    Time: 1 ms (99.91%) | Memory: 46 MB (21.23%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,36 +1,17 @@
 class Solution {
     public boolean canJump(int[] nums) {
-       
-     
+        int n = nums.length;
 
-    int n = nums.length; 
+        int flag = n-1;
 
-    int index =n-1;
-    while(index>=0){
-        boolean validEle = false;
-
-        if(nums[index]==0&&index!=n-1){
-            for(int i=index-1;i>=0;i--){
-                if(nums[i]>=index-i+1){
-                    index=i;
-
-                    validEle = true;
-                    break;
-                }
-
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]+i>=flag){
+                flag = i;
             }
-             if(validEle==false){
-                return false;
-             }
         }
-        index--;
-
+        if(flag==0)
+            return true;
+    
+        return false;
     }
-
-    return true;
-
-
-    }
-
-
 }
