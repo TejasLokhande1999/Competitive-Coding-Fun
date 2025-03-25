daf7764f462e40c4906f7dea99b08503db54540b
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2025-03-25 08:31:48 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (88.20%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,25 +1,21 @@
 class Solution {
     int dp[];
     public int rob(int[] nums) {
-        
         int n = nums.length;
         dp = new int[n];
         Arrays.fill(dp,-1);
         return dfs(0,n,nums);
-
     }
 
-    public int dfs(int i, int n, int nums[]){
+    public int dfs(int i, int n, int cost[]){
 
         if(i>=n){
             return 0;
         }
-
         if(dp[i]!=-1){
             return dp[i];
         }
 
-        return dp[i] = Math.max(nums[i]+dfs(i+2,n,nums),dfs(i+1,n,nums));
-
+        return dp[i] =  Math.max(cost[i]+dfs(i+2,n,cost),dfs(i+1,n,cost));
     }
 }
