c5532e8d674217120eec0ab910ddbb2468cd5ebe
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2025-03-26 07:22:17 +0530

    Time: 11 ms (20.05%) | Memory: 60.6 MB (36.21%) - LeetSync

--- 221-maximal-square/maximal-square.java
+++ 221-maximal-square/maximal-square.java
@@ -1,17 +1,16 @@
 class Solution {
-    private int dp[][];
+
+    int dp[][];
     public int maximalSquare(char[][] matrix) {
-        
         int rows = matrix.length;
         int cols = matrix[0].length;
 
         dp = new int[rows][cols];
 
         for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                dp[i][j] = -1;
-            }
+            Arrays.fill(dp[i],-1);
         }
+
         dfs(0,0,rows,cols,matrix);
 
         int maxVal = -1;
@@ -23,27 +22,30 @@ class Solution {
         }
 
         return maxVal*maxVal;
+    
     }
 
+    public int dfs(int i, int j, int rows, int cols, char[][] matrix){
 
-    public int dfs(int i, int j, int rows, int cols, char matrix[][]){
+        if(i>=rows || j>=cols){
+            return 0;
+        }
 
-            if(i>=rows || j>=cols){
-                return 0;
-            }
-
-            if(dp[i][j]!=-1){
-                return dp[i][j];
-            }
-
-            int right = dfs(i,j+1,rows,cols,matrix);
-            int down = dfs(i+1,j,rows,cols,matrix);
-            int diag = dfs(i+1,j+1,rows,cols,matrix);
-
-            dp[i][j] = 0;
-            if(matrix[i][j]=='1'){
-                dp[i][j] = 1 + Math.min(right,Math.min(down,diag));
-            }
+        if(dp[i][j]!=-1){
             return dp[i][j];
+        }
+
+        int right = dfs(i,j+1,rows,cols,matrix);
+        int down = dfs(i+1,j,rows,cols,matrix);
+        int diag = dfs(i+1,j+1,rows,cols,matrix);
+
+        
+        dp[i][j] = 0;
+        if(matrix[i][j]=='1'){
+            dp[i][j] = 1 + Math.min(right, Math.min(down,diag));
+        }
+
+        return dp[i][j];
+
     }
 }
