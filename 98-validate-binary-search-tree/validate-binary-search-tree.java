ad071e398f8e226d3ffdfd5f70392aae60979522
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2025-03-28 02:37:30 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (67.80%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -15,21 +15,22 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        
         return inorder(root,Long.MIN_VALUE, Long.MAX_VALUE);
     }
 
-    public boolean inorder(TreeNode node, long min,long max){
+    public boolean inorder(TreeNode node, long min, long max){
 
         if(node==null){
             return true;
         }
 
-        if(node.val<=min || node.val>=max){
-            return false;
+        if(min< node.val && node.val<max){
+
+            return inorder(node.left,min,node.val) && inorder(node.right,node.val,max);
         }
 
-        return inorder(node.left,min,node.val)&&inorder(node.right,node.val,max);
+        return false;
+
 
     }
 }
