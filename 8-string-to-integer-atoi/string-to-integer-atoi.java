d0c428fd9498e2972df8dc1c42f810367b222124
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2025-03-27 11:52:41 +0530

    Time: 1 ms (100.00%) | Memory: 42.3 MB (80.51%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,57 +1,39 @@
 class Solution {
     public int myAtoi(String s) {
-        
-        if(s.isEmpty()){
-            return 0;
-        }
-        s=s.trim();
-
-        if(s.isEmpty()){
-            return 0;
-        }
+        s = s.trim();
         int sign = 1;
         int index=0;
+        int n = s.length();
+        if(s.isEmpty()){
+            return 0;
+        }
         if(s.charAt(0)=='-'){
             sign = -1;
             index=1;
         }else if(s.charAt(0)=='+'){
+            sign = 1;
             index=1;
         }
 
+        long num=0;
+        
 
-        long ans=0;
-
-
-        for(int i=index;i<s.length();i++){
-
+        for(int i=index;i<n;i++){
             char ch = s.charAt(i);
-
             if(Character.isDigit(ch)){
-                int digit = ch-'0';
-                ans = ans*10 + digit;
-
-                if (sign == 1 && ans > Integer.MAX_VALUE) {
-                return Integer.MAX_VALUE;
-            }
-            if (sign == -1 && -ans < Integer.MIN_VALUE) {
-                return Integer.MIN_VALUE;
-            }
-
+                num = num*10 + ch-'0';
+            
+                if(sign==-1 && -num<Integer.MIN_VALUE){
+                    return Integer.MIN_VALUE;
+                }else if(sign==1 && num>Integer.MAX_VALUE){
+                    return Integer.MAX_VALUE;
+                }
             }else{
                 break;
             }
 
         }
 
-        if(sign==-1){
-            ans = ans*-1;        
-                return (int)ans;
-        }else{
-            
-                return (int)ans;
-            
-        }
-
-
+        return (int)num*sign;
     }
 }
