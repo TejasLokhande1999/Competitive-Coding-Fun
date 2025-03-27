b48c87eadafb715dc9b03301ca7f65f7c45530a0
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2025-03-28 01:51:07 +0530

    Time: 2 ms (85.37%) | Memory: 51.8 MB (98.08%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.java
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.java
@@ -14,18 +14,15 @@
  * }
  */
 class Solution {
-    public int count=0;
+    int count=0;
     public int goodNodes(TreeNode root) {
         
         height(root,Integer.MIN_VALUE);
 
         return count;
-
-
     }
 
-    public void height(TreeNode node , int max){
-
+    public void height(TreeNode node,int max){
         if(node==null){
             return;
         }
@@ -33,11 +30,9 @@ class Solution {
         if(node.val>=max){
             count+=1;
         }
-
         max = Math.max(max,node.val);
 
         height(node.left,max);
         height(node.right,max);
-
     }
 }
