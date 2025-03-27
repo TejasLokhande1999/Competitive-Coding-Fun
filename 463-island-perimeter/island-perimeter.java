d28da75ddce2c14a09f006e2303976661e178566
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2025-03-28 03:13:10 +0530

    Time: 8 ms (21.81%) | Memory: 46.4 MB (10.21%) - LeetSync

--- 463-island-perimeter/island-perimeter.java
+++ 463-island-perimeter/island-perimeter.java
@@ -2,20 +2,23 @@ class Solution {
     public int islandPerimeter(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
+
         boolean visited[][] = new boolean[m][n];
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]==1){
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+
+                if(grid[i][j]==1 && !visited[i][j]){
                     return dfs(i,j,grid,visited);
                 }
+
             }
         }
 
         return 0;
-
     }
 
-    public int dfs(int i, int j, int grid[][],boolean visited[][]){
+    public int dfs(int i, int j, int [][] grid, boolean visited[][]){
 
         int rows = grid.length;
         int cols = grid[0].length;
@@ -23,15 +26,11 @@ class Solution {
         if(i<0 || i>=rows || j<0 || j>=cols || grid[i][j]==0){
             return 1;
         }
-
-        //check if visited. If visited, then rerurn 0
         if(visited[i][j]){
             return 0;
         }
 
-        //marked as visited
-        visited[i][j]=true;
-
+        visited[i][j] = true;
         int p=0;
         p+=dfs(i+1,j,grid,visited);
         p+=dfs(i-1,j,grid,visited);
