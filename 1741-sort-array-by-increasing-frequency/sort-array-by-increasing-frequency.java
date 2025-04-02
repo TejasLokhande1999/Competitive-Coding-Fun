5d7e75efce71f917fd5cd5e2067927857b72be45
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2025-04-02 09:14:02 +0530

    Time: 5 ms (94.23%) | Memory: 44.7 MB (34.23%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
@@ -1,40 +1,49 @@
 class Solution {
     public int[] frequencySort(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
+         Map<Integer, Integer> map = new HashMap<>();
 
         for(int i=0;i<nums.length;i++){
             map.put(nums[i], map.getOrDefault(nums[i],0)+1);
         }
 
-    //Npw we have the frequesncy
+        int n = nums.length;
 
     ArrayList<Pair> list = new ArrayList<>();
 
-    //. <ele, freqy>
-    for(Map.Entry<Integer, Integer> entry : map.entrySet()){
-        list.add(new Pair(entry.getKey(),entry.getValue()));
-    }
+    
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+            list.add(new Pair(entry.getKey(),entry.getValue()));
+        }
 
-    Collections.sort(list, new PairComparator());
-    //[(ele, freq),(ele,freq)]
-    // [(),()]
+    Collections.sort(list, (a,b)->{
 
-    int arr[] = new int[nums.length];
-    int index=0;
+        if(a.freq!=b.freq){
+            return a.freq-b.freq;
+        }
+        //else 
+
+        return b.ele-a.ele;
+
+    });
+
+    int retArr[] = new int[n];
+
+    int index = 0;
+
     for(int i=0;i<list.size();i++){
-        Pair p1 = list.get(i);
-        int ele = p1.ele;
-        int freq = p1.freq;
-
+        Pair p = list.get(i);
+        int freq =p.freq;
+        int ele = p.ele;
         for(int j=0;j<freq;j++){
-            arr[index] = ele;
-            index++;
+            retArr[index] = ele;
+            index+=1;
         }
     }
+    
 
-    return arr;
+    return retArr;
+
     }
-
 }
 
 class Pair{
@@ -43,29 +52,5 @@ class Pair{
     Pair(int ele, int freq){
         this.ele = ele;
         this.freq= freq;
-    }
-}
-
-class PairComparator implements Comparator<Pair>{
-
-    public int compare(Pair p1, Pair p2){
-
-        if(p1.freq>p2.freq){
-            return 1;
-        }else if(p1.freq<p2.freq){
-            return -1;
-        }else{      //p1.freq==p2.freq
-                //nor sprt in descending order of their value
-
-                if(p1.ele>p2.ele){
-                    return -1;
-                }else if(p1.ele<p2.ele){
-                    return 1;
-                }else{      //p1.ele ==p2.ele
-                    return 0;
-                }
-
-        }
-
     }
 }
