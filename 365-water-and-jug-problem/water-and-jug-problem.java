d4fe3a4634f7d1c9caabc763ae4289576896a612
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2025-04-04 06:38:02 +0530

    Time: 0 ms (100.00%) | Memory: 40 MB (97.86%) - LeetSync

--- 365-water-and-jug-problem/water-and-jug-problem.java
+++ 365-water-and-jug-problem/water-and-jug-problem.java
@@ -1,28 +1,33 @@
 class Solution {
     public boolean canMeasureWater(int x, int y, int target) {
-        Set<String> set = new HashSet<>();
-        return dfs(0,0,x,y,target,set);
-    }
-
-    public boolean dfs(int j1, int j2, int x, int y, int target, Set<String> set){
-
-        if(j1+j2==target){
-            return true;
-        }
         
-        String key = j1+","+j2;
-
-        //we see if this state was previosuly present or not
-        if(set.contains(key))
+        if(x+y<target){
             return false;
 
-        set.add(key);
+        }
+        int gcd = gcd(x,y);
+        System.out.println(gcd);
+        if(target%gcd==0)
+            return true;
+        
 
-        return  dfs(x,j2,x,y,target,set) ||      //fill j1
-                dfs(j1,y,x,y,target,set) ||     //fill j1
-                dfs(0,j2,x,y,target,set) ||        //empty j1
-                dfs(j1,0,x,y,target,set) ||           //empty j2
-                dfs(Math.max(0,j1-(y-j2)),Math.min(y,j1+j2),x,y,target,set) ||        //pour from j1->j2
-                dfs(Math.min(x,j1+j2),Math.max(0,j2-(x-j1)),x,y,target,set);       //from j2-->j1
+        return false;
+    }
+
+    public int gcd(int num1, int num2){
+
+        int no = Math.min(num1,num2);
+
+        for(int i=no;i>=2;i--){
+
+            if(num1%i==0 && num2%i==0){
+                return i;
+            }
+
+        }
+
+        return 1;
+
+
     }
 }
