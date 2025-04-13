cf8b464e7b3e0067e78ab820be205c89ea0e95b0
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2025-04-13 13:32:25 +0530

    Time: 9 ms (69.53%) | Memory: 46.3 MB (19.30%) - LeetSync

--- 924-fair-candy-swap/fair-candy-swap.java
+++ 924-fair-candy-swap/fair-candy-swap.java
@@ -1,40 +1,56 @@
 class Solution {
     public int[] fairCandySwap(int[] A, int[] B) {
         
+
+        // alice = [1,1]. aliceSum = 2;
+        // bob = [2,2],bobSum = 4;
+
+        // diff = -1;
+
+
+
+        // totalSumA - a[i] + b[j] = totalSumB-b[j]+a[i];
+        // -2 a[i] + 2b[j] = totalSumB-totalSumA;
+        // a[i] - b[j] = (totalSumA-totalSumB)/2;
+
+        // a = diff + b;
+
         int sumA = 0;
         int sumB = 0;
 
-        for(int i=0;i<A.length;i++){
-            sumA+=A[i];
+        for(int num1 : A){
+            sumA +=num1;
         }
 
-        for(int i=0;i<B.length;i++){
-            sumB+=B[i];
-        }
 
+        for(int num2 : B){
+            sumB +=num2;
+        }
 
         int diff = (sumA-sumB)/2;
 
-        int retArr[] = new int[2];
-
         Set<Integer> set = new HashSet<>();
 
-        for(int a: A){
-            set.add(a);
+
+        //if in b, we find a then return that
+
+        for(int i=0;i<A.length;i++){
+            set.add(A[i]);
         }
 
-        for(int b : B){
-            int reqNo = b+diff;
 
-            if(set.contains(reqNo)){
-                retArr[0] = reqNo;
-                retArr[1] = b;
+        for(int i=0;i<B.length;i++){
+            int ele = diff+B[i];
 
-                return retArr;
+            if(set.contains(ele)){
+
+                return new int[]{ele,B[i]};
             }
         }
 
-        return retArr;
+
+        return new int[] {0,0};
+
 
     }
 }
