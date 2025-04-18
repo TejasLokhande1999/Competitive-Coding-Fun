cebeb0f313d9cc72a3279f876f320197bd8ad0b5
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2025-04-18 07:26:17 +0530

    Time: 1 ms (100.00%) | Memory: 42.7 MB (62.17%) - LeetSync

--- 415-add-strings/add-strings.java
+++ 415-add-strings/add-strings.java
@@ -1,33 +1,30 @@
 class Solution {
     public String addStrings(String num1, String num2) {
-
-     int i = num1.length()-1;
-     int j = num2.length()-1;
-     
-     int sum=0;
-     int carry = 0;
-     int finalSum = 0;
-     StringBuilder str = new StringBuilder();
-
-     while(i>=0 || j>=0){
-
-        int n1 = (i>=0) ? num1.charAt(i)-'0' : 0;
-        int n2 = (j>=0) ? num2.charAt(j)-'0' : 0;
-
-        sum = n1+n2+carry;
-
-        carry = sum/10;         //for next iteration
-        finalSum = sum%10;
-        str.append(finalSum);
-
-        i--;
-        j--;
-     
-     }
-     if(carry>0)
-        str.append(carry);
         
-     return str.reverse().toString();
+        int i = num1.length()-1;
+        int j = num2.length()-1;
+
+        StringBuilder str = new StringBuilder();
+        int carry = 0;
+        int sum=0;
+        while(i>=0 || j>=0){
+            
+            int n1 = (i>=0) ? num1.charAt(i)-'0' : 0;
+            int n2 = (j>=0) ? num2.charAt(j)-'0' : 0;
+
+            sum = n1+n2+carry;
+            carry = sum/10;
+            str.append(sum%10);
+
+            i--;
+            j--;
+
+        }
+
+        if(carry>0)
+            str.append(carry);
+
+        return str.reverse().toString();
 
     }
 }
