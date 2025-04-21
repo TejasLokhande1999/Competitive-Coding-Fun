1d3633ad6a6005250e6d223c282c81d9e8345157
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2025-04-21 07:33:18 +0530

    Time: 41 ms (8.02%) | Memory: 54.3 MB (6.64%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -1,39 +1,35 @@
 class Solution {
     public int firstUniqChar(String s) {
-        
-        Map<Character, Integer> map = new LinkedHashMap<Character, Integer>();
-        char arr[] = s.toCharArray();
-        char ch='0';
+        Queue<Pair> q = new LinkedList<>();
+        Map<Character,Integer> map = new HashMap<>();
 
-        for(int i=0;i<s.length();i++)
-        {
-            if(map.containsKey(arr[i]))
-            {
-                int val = map.get(arr[i]);
-                map.put(arr[i],val+1);
-            }else{
-                map.put(arr[i],1);
-            }
-        }
-        for(Map.Entry<Character,Integer> entry : map.entrySet())
-        {
-            int val = entry.getValue();
-            if(val==1)
-            {
-             ch = entry.getKey();
-                break;
-            }
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            q.add(new Pair(ch,i));
+            map.put(ch, map.getOrDefault(ch,0)+1);
         }
 
-        for(int i=0;i<arr.length;i++)
-        {
-            if(ch==arr[i])
-            {
-                return i;
-            }
+        while(!q.isEmpty()){
+
+            Pair p = q.poll();
+            char ch = p.ch;
+            int index = p.index;
+
+            int freq = map.get(ch);
+            if(freq==1)
+                return index;
+            
         }
 
+        return -1;
 
-return -1;
+    }
+}
+class Pair{
+    char ch;
+    int index;
+    Pair(char ch, int index){
+        this.ch = ch;
+        this.index = index;
     }
 }
