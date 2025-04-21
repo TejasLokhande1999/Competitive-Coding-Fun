b0b4ad5f52901c54745d4cf22e81e109197236ab
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2025-04-21 06:14:14 +0530

    Time: 3 ms (23.53%) | Memory: 44.8 MB (46.74%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,30 +1,32 @@
 class Solution {
-    List<List<Integer>> res = new ArrayList<>();
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        
-        ArrayList<Integer> list = new ArrayList<>();
-        dfs(candidates, target, 0, list);
+        Arrays.sort(candidates);
+
+        List<List<Integer>> res = new ArrayList<>();
+
+        List<Integer> curr = new ArrayList<>();
+
+        backtrack(0,candidates,res,curr,target);
+
         return res;
     }
 
-        public void dfs(int arr[], int target, int index, ArrayList<Integer> list){
-            if(target==0){
-                res.add(new ArrayList<>(list));
-            }else if(target<0 || index ==arr.length){
-                return;
-            }else{
-                list.add(arr[index]);
+    public void backtrack(int index, int[] candidates, List<List<Integer>> res, List<Integer> curr, int target){
+
+        if(target<0)
+            return;
         
-                dfs(arr,target-arr[index],index,list);
-
-                list.remove(list.size()-1);
-
-                dfs(arr,target,index+1,list);
-
-            }
+        if(target==0){
+            res.add(new ArrayList<>(curr));
         }
 
+        for(int i=index; i<candidates.length;i++){
+            curr.add(candidates[i]);
+            backtrack(i,candidates,res,curr,target-candidates[i]);
+            curr.remove(curr.size()-1);
+        }
 
+        
 
-
+    }
 }
