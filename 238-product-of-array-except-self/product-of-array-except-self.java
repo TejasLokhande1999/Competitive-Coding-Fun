fa838be5a4cfce6032a44f4e3bbd461f170d93a9
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2025-04-21 06:30:17 +0530

    Time: 2 ms (88.00%) | Memory: 56.3 MB (15.81%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,31 +1,38 @@
 class Solution {
-    public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        int prefix[] = new int [n];
-        int postfix[] = new int[n];
+    public int[] productExceptSelf(int[] arr) {
 
-        prefix[0] = nums[0];
+        int n = arr.length;
+        int prod[] = new int[n];
+
+        int pre[] = new int[n];
+        int suf[] = new int[n];
+
+
+        pre[0] = 1;
+
         for(int i=1;i<n;i++){
-            prefix[i] = nums[i]*prefix[i-1];
+            pre[i] = pre[i-1]*arr[i-1];
         }
-
-        postfix[n-1]=nums[n-1];
+        
+        suf[n-1]=1;
         for(int i=n-2;i>=0;i--){
-            postfix[i] = nums[i]*postfix[i+1];
+            suf[i] = suf[i+1]*arr[i+1];
         }
-
-        int op[] = new int[n];
 
         for(int i=0;i<n;i++){
-            if(i==0){
-                op[i] = 1*postfix[i+1];
-            }else if(i==n-1){
-                op[i] = prefix[i-1]*1;
-            }else{
-                op[i] = prefix[i-1]*postfix[i+1];
-            }
+            prod[i] = pre[i]*suf[i];
         }
 
-        return op;
+        return prod;
+
+
     }
 }
+
+
+
+
+// arr[]       1   2   3   4
+// prefix[]    1   1   2   6       prefix[i] = prefix[i-1]*arr[i-1]
+// suffix[]    24  12  4   1
+// op[]        1   12  8   6
