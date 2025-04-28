92efbb7df9787187a0cc49f2168130d966e79a18
Tejas <65606344+TejasLokhande1999@users.noreply.github.com>
2025-04-28 13:33:20 +0530

    Time: 23 ms (27.82%) | Memory: 45.6 MB (65.44%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,65 +1,53 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
-        
-
-        Queue<Pair> q= new LinkedList<>();
-        Pair pair = new Pair(0,0,1);
-        q.add(pair);
         int n = grid.length;
-        //int n = grid[0].length;
 
-        if(grid[0][0]==1 || grid[n-1][n-1]==1){
+        if(grid[0][0]==1)
             return -1;
-        }
+
+        if(grid[n-1][n-1]==1)
+            return -1;
+
+        Queue<Pair> q = new LinkedList<>();
         boolean visited[][] = new boolean[n][n];
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                visited[i][j] = false;
-            }
-        }
+        q.add(new Pair(0,0,1));
 
         while(!q.isEmpty()){
 
             Pair p = q.poll();
-
-            int x = p.x;
-            int y = p.y;
-            int val = p.val;
+            int x= p.x;
+            int y= p.y;
+            int val= p.val;
 
             if(x==n-1 && y==n-1){
                 return val;
             }
 
-            int directions[][] ={{-1,1},
-                                 {0,1},
-                                 {1,1},
-                                 {1,0},
-                                 {1,-1},
-                                 {0,-1},
-                                 {-1,-1},
-                                 {-1,0}};
+            //else
 
 
+        int dirs[][] = {{0,1},{0,-1},{1,0},{-1,0},{1,1},{1,-1},{-1,1},{-1,-1}};
 
-            for(int i=0;i<8;i++){
-                int row = x+directions[i][0];
-                int col = y+directions[i][1];
+        for(int dir[] : dirs){
 
-                if(row>=0 && row<=n-1 && col>=0 && col<=n-1 && 
-                !visited[row][col] && grid[row][col]==0){
-                    q.add(new Pair(row,col,val+1));
-                    visited[row][col] = true;
-                }
+            int px = x+dir[0];
+            int py = y+dir[1];
 
+            if(px>=0 && px<n && py>=0 && py<n && !visited[px][py] && grid[px][py]==0){
+
+                q.add(new Pair(px,py,val+1));
+                visited[px][py]=true;
             }
-
 
         }
 
+        }
 
         return -1;
 
+
+        
     }
 }
 
@@ -75,9 +63,5 @@ class Pair{
         this.val = val;
     }
 
+
 }
-
-
-    // -1,1      0,1     1,1
-    // -1,0             1,0
-    // -1,-1     0,-1    1,-1
